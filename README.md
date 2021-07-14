5668faeddaeb0a9881926b52aef37f6113ba12bf
emc-re-ds <enriquemorales.re.ds@gmail.com>
2021-07-14 15:05:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# Scrollerfy 
+
+## Table Of Contents
+* [General Info](#general-info)
+* [Prerequisites](#prerequisites)
+* [Module Example](#prerequisites)
+* [Custom Arguments](#prerequisites)
+* [License](#license)
+
+### General Info
+This module shows the information of the song that is currently playing on Spotify and scrolls it if the characters are longer that a certain number configured or if the user does not want any scroll.
+
+[![sample screenshot](https://i.imgur.com/kEluTSq.png)
+
+### Prerequisites
+- Python (2.x or 3.x)
+- Python `dbus` module
+
+### Module Example
+[module/spotify]
+type = custom/script
+tail = true
+format = <label>
+label = %output%
+click-left = playerctl play-pause spotify
+;exec = python -u /path/to/spotify/script -s -t 35 -f '{artist}: {song}' -p '[playing],[paused]'
+exec-if = pgrep -x spotify
+exec = ~/bin/init_polySpotify.sh
+format-foreground = #1db954
+interval = 1
+
+### Custom Arguments
+
+##### Truncate
+
+The argument "-t" is optional and sets the `trunlen`. It specifies the maximum length of the printed string, so that it gets truncated when the specified length is exceeded. Defaults to 35.
+
+Override example:
+
+``` ini
+exec = python /path/to/spotify/script -t 42
+```
+
+##### Format
+
+The argument "-f" is optional and sets the format. You can specify how to display the song and the artist's name, as well as where (or whether) to print the play-pause indicator. 
+
+Override example:
+
+``` ini
+exec = python /path/to/spotify/script -f '{play_pause} {song} - {artist} - {album}'
+```
+
+This would output "Lone Digger - Caravan Palace - <I°_°I>" in your polybar, instead of what is shown in the screenshot.
+
+##### Status indicator
+
+The argument "-p" is optional, and sets which unicode symbols to use for the status indicator. These should be given as a comma-separated string, with the play indicator as the first value and the pause indicator as the second.
+
+Override example:
+
+``` ini
+exec = python /path/to/spotify/script -p '[playing],[paused]'
+```
+
+##### Fonts
+
+The argument "--font" is optional, and allow to specify which font from your Polybar config to use to display the main label.
+
+Override example:
+```ini
+exec = python /path/to/spotify/script --font=1
+```
+
+The argument "--playpause-font" is optional, and allow to specify which font from your Polybar config to use to display the "play/pause" indicator.
+
+Override example:
+``` ini
+exec = python /path/to/spotify/script -p '[playing],[paused]' --playpause-font=2
+```
+
+##### Quiet
+
+The argument "-q" or "--quiet" is optional and specifies whether to display the output when the current song is paused.
+This will make polybar only show a song title and artist (or whatever your custom format is) when the song is actually playing and not when it's paused.
+Simply setting the flag on the comand line will enable this option.
+
+Override example:
+```ini
+exec = python /path/to/spotify/script -q
+```
+
+##### Scroll
+
+The argument "-s" or "--scroll" is optional, allows scrolling of the text when the specified length is exceeded.
+
+Override example:
+```ini
+exec = python /path/to/spotify/script -s
+```
+
+### License
+SRCompiler is Free Software: You can use, study share and improve it at your will. Specifically you can redistribute and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
---
+++ scrollerfy.py
@@ -0,0 +1,234 @@
+"""
+    This file is part of Scrollerfy.
+    Scrollerfy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Scrollerfy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Scrollerfy.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+#!/usr/bin/env python
+
+import time 
+import sys
+import dbus
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-t',
+    '--trunclen',
+    type=int,
+    metavar='trunclen'
+)
+parser.add_argument(
+    '-f',
+    '--format',
+    type=str,
+    metavar='custom format',
+    dest='custom_format'
+)
+parser.add_argument(
+    '-p',
+    '--playpause',
+    type=str,
+    metavar='play-pause indicator',
+    dest='play_pause'
+)
+parser.add_argument(
+    '--font',
+    type=str,
+    metavar='the index of the font to use for the main label',
+    dest='font'
+)
+parser.add_argument(
+    '--playpause-font',
+    type=str,
+    metavar='the index of the font to use to display the playpause indicator',
+    dest='play_pause_font'
+)
+parser.add_argument(
+    '-q',
+    '--quiet',
+    action='store_true',
+    help="if set, don't show any output when the current song is paused",
+    dest='quiet',
+)
+parser.add_argument(
+    '-s',
+    '--scroll',
+    action='store_true',
+    help="if set, scrolls the text",
+    dest='scroll_text',
+)
+
+
+args = parser.parse_args()
+
+
+def fix_string(string):
+    # corrects encoding for the python version used
+    if sys.version_info.major == 3:
+        return string
+    else:
+        return string.encode('utf-8')
+
+def truncate(name, trunclen):
+    if len(name) > trunclen:
+        scroll = 1
+        name = name[:trunclen]
+        name += '...'
+        if ('(' in name) and (')' not in name):
+            name += ')'
+    return name
+
+def scroller(name, song, play_pause, scroll_text, trunclen):
+    current_state = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+
+    if (scroll_text == False or 
+            len(name) <= trunclen or 
+            current_state == 'Paused'):
+        while(True):
+            metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+            song_actual = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
+            
+            # call again the script if the song changes
+            if (song != song_actual):
+                return False
+            
+            # If the song is playing again and scrolling is activated, then change
+            if (current_state != spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')):
+                return False
+            
+            # If the scroll is desactivated
+            if (len(name) > trunclen):
+                name = name[:trunclen]
+                name += '...'
+                if ('(' in name) and (')' not in name):
+                    name += ')'
+            
+            print(play_pause, name, sep=' ')
+            time.sleep(0.25) 
+    else:
+        marker1 = 0
+        marker2 = 1
+        marker3 = 2
+        while(True):    
+            if (spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus') == 'Paused'):
+                return False
+            else:
+                metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+                song_actual = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
+            
+                if (song != song_actual):
+                    return False
+            
+                if (marker1+trunclen <= len(name)):
+                    text_head = name[marker1:trunclen+marker1]
+                else:
+                    text_head = name[marker1:len(name)]
+               
+                if (marker1 >= len(name)):
+                    transition_char = " " + marker3*"<" + " "
+                    text_tail = name[0:marker2]
+                    marker3 = marker3-1
+
+                    print(play_pause, transition_char, text_tail, sep='')
+                elif (abs(marker1-len(name)) < trunclen):
+                    text_tail = name[0:marker2]
+                    marker2 = marker2+1
+
+                    print(play_pause, text_head, "<<" ,text_tail, sep=' ')
+                else:
+                    text_tail = ''
+                    
+                    print(play_pause, text_head, sep=' ')
+
+                marker1 = marker1+1
+
+                if (marker1 == len(name)+2):
+                    marker1 = 0
+                    marker2 = 1
+                    marker3 = 2
+
+                time.sleep(0.25) 
+
+# Default parameters
+output = fix_string(u'{play_pause} {artist}: {song}')
+trunclen = 35
+play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
+
+label_with_font = '%{{T{font}}}{label}%{{T-}}'
+font = args.font
+play_pause_font = args.play_pause_font
+
+quiet = args.quiet
+scroll_text = args.scroll_text
+
+# parameters can be overwritten by args
+if args.trunclen is not None:
+    trunclen = args.trunclen
+if args.custom_format is not None:
+    output = args.custom_format
+if args.play_pause is not None:
+    play_pause = args.play_pause
+
+try:
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object(
+        'org.mpris.MediaPlayer2.spotify',
+        '/org/mpris/MediaPlayer2'
+    )
+
+    spotify_properties = dbus.Interface(
+        spotify_bus,
+        'org.freedesktop.DBus.Properties'
+    )
+
+    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+
+    # Handle play/pause label
+
+    play_pause = play_pause.split(',')
+
+    if status == 'Playing':
+        play_pause = play_pause[0]
+    elif status == 'Paused':
+        play_pause = play_pause[1]
+    else:
+        play_pause = str()
+
+    if play_pause_font:
+        play_pause = label_with_font.format(font=play_pause_font, label=play_pause)
+
+    # Handle main label
+
+    artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'] else ''
+    song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
+    album = fix_string(metadata['xesam:album']) if metadata['xesam:album'] else ''
+
+    if (quiet and status == 'Paused') or (not artist and not song and not album):
+        print('')
+    else:
+        if font:
+            artist = label_with_font.format(font=font, label=artist)
+            song = label_with_font.format(font=font, label=song)
+            album = label_with_font.format(font=font, label=album)
+    
+    name = output.format(artist=artist, 
+            song=song,
+            album=album)
+ 
+    scroller(name, song, play_pause, scroll_text, trunclen + 4) 
+
+except Exception as e:
+    if isinstance(e, dbus.exceptions.DBusException):
+        print('')
+    else:
+        print(e)
